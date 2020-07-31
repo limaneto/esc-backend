ecfb5af466ea864ea4db686540185733c9ce2ee0
lima neto <limanetocc@gmail.com>
2020-07-31 18:27:37 -0300

    Add allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x4&!^ihn@c_-n7e-s2u)gx&ca#t)#wu4u++c#1obbv3d5vyrz5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
