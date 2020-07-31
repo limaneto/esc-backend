8cf318c09f94bd8c1266c39f79dd966c0cedd7f0
lima neto <limanetocc@gmail.com>
2020-07-31 18:34:46 -0300

    Get virtual env

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x4&!^ihn@c_-n7e-s2u)gx&ca#t)#wu4u++c#1obbv3d5vyrz5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
