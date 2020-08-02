74880c7786585f895d1a60411c3089b15b3e821b
lima neto <limanetocc@gmail.com>
2020-08-02 16:09:37 -0300

    Change

--- app/views.py
+++ app/views.py
@@ -4,5 +4,5 @@ import datetime
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    html = "<html><body>Currently is now %s.</body></html>" % now
     return HttpResponse(html)
